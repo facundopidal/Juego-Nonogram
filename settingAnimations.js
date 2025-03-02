11964858cb24b4f11a8628fed962e1077a948780
Facundo Pidal <facundopidalruizdiaz@gmail.com>
2025-03-02 12:17:16 -0300

    cambios de formato

--- settingAnimations.js
+++ settingAnimations.js
@@ -4,12 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeButton = document.querySelector('.close-dialog button');
     const errorElements = document.querySelectorAll('.error');
 
-    // Mostrar el diálogo con animación usando clases CSS
+
     settingsButton.addEventListener('click', () => {
         settingsDialog.classList.add('open');
     });
 
-    // Cerrar el diálogo con animación usando clases CSS
     closeButton.addEventListener('click', () => {
         settingsDialog.classList.remove('open');
     });
@@ -30,30 +29,23 @@ newGameButton.addEventListener('click', () => {
 });
 
 
-// Seleccionamos el checkbox del modo oscuro
 const darkModeToggle = document.getElementById("darkmode-check");
 
-// Agregamos un evento al cambio del checkbox
 darkModeToggle.addEventListener("change", () => {
-  // Verifica si el checkbox está marcado
   if (darkModeToggle.checked) {
-    // Agregar la clase 'dark-mode' al body
     document.body.classList.add("dark-mode");
   } else {
-    // Remover la clase 'dark-mode'
     document.body.classList.remove("dark-mode");
   }
 });
 
 
-// Guardar estado en localStorage
 const savedTheme = localStorage.getItem("darkMode");
 
 if (savedTheme === "enabled") {
     enableDarkMode(); // Activar al cargar
 }
 
-// Activar modo oscuro
 function enableDarkMode() {
     darkModeToggle.checked = true;
     document.body.classList.add("dark-mode");
@@ -63,7 +55,6 @@ function enableDarkMode() {
     localStorage.setItem("darkMode", "enabled");
 }
 
-// Desactivar modo oscuro
 function disableDarkMode() {
     darkModeToggle.checked = false;
     document.body.classList.remove("dark-mode");
@@ -73,7 +64,6 @@ function disableDarkMode() {
     localStorage.setItem("darkMode", "disabled");
 }
 
-// Escuchar cambios en el checkbox
 darkModeToggle.addEventListener("change", () => {
     if (darkModeToggle.checked) {
         enableDarkMode();
